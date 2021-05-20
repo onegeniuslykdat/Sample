4ed17da4964237f5ef9307074a9294a7b4ae9cf3
onegeniuslykdat <27788258+onegeniuslykdat@users.noreply.github.com>
2021-05-20 11:48:22 +0100

    Create welcome.js

---
+++ welcome.js
@@ -0,0 +1,11 @@
+ let names = document.getElementById("listNames")
+    let msg = document.getElementById("Welcome")
+    
+    names.addEventListener('change', () => {
+      if(names.value == "0") {
+         msg.innerText = "Welcome";
+      }
+      else {
+   msg.innerText = "Welcome, " + names.selectedOptions[0].innerText;
+      }
+})
